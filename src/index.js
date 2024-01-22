394e09128c36935be3624bc727a44c3a93541ce9
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-01-22 16:47:58 +0300

    popup edit, add, image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import {initialCards} from './scripts/cards.js';
-import './pages/index.css';
+import { initialCards } from "./scripts/cards.js";
+import "./pages/index.css";
 
 const content = document.querySelector(".content");
 const cardTemplate = document.querySelector("#card-template").content;
 const placesContent = content.querySelector(".places__list");
-const addButton = content.querySelector(".profile__add-button");
 
+const imagePopup = document.querySelector(".popup_type_image");
 
 function makeCard(name, link) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
@@ -19,6 +19,12 @@ function makeCard(name, link) {
     onDelButton(cardElement);
   });
 
+  cardElement.addEventListener("click", function () {
+    imagePopup.style.display = "flex";
+    imagePopup.querySelector(".popup__image").src = link;
+    imagePopup.querySelector(".popup__image").alt = name;
+  });
+
   return cardElement;
 }
 
@@ -29,3 +35,60 @@ function onDelButton(card) {
 initialCards.forEach((item) => {
   placesContent.append(makeCard(item.name, item.link));
 });
+
+//edit Button
+const editButton = content.querySelector(".profile__edit-button");
+const editPopup = document.querySelector(".popup_type_edit");
+editButton.addEventListener("click", function () {
+  const profileName = document.querySelector(".profile__title");
+  const profileDescription = document.querySelector(".profile__description");
+  editPopup.style.display = "flex";
+  document.querySelector(".popup__input_type_name").placeholder =
+    profileName.innerHTML;
+  document.querySelector(".popup__input_type_description").placeholder =
+    profileDescription.innerHTML;
+});
+
+const closeEditPopup = document.querySelector(".popup__close");
+closeEditPopup.addEventListener("click", function () {
+  editPopup.style.display = "none";
+});
+
+editPopup.addEventListener("click", function () {
+  editPopup.style.display = "none";
+});
+
+//add Button
+const addButton = content.querySelector(".profile__add-button");
+const addPopup = document.querySelector(".popup_type_new-card");
+const closeAddPopup = document.querySelector(
+  ".popup_type_new-card .popup__close"
+);
+addButton.addEventListener("click", function () {
+  addPopup.style.display = "flex";
+});
+
+closeAddPopup.addEventListener("click", function () {
+  addPopup.style.display = "none";
+});
+
+addPopup.addEventListener("click", function () {
+  addPopup.style.display = "none";
+});
+
+document.body.addEventListener("keydown", function (e) {
+  if (e.code == "Escape") {
+    editPopup.style.display = "none";
+    addPopup.style.display = "none";
+    imagePopup.style.display = "none";
+  }
+});
+
+const closeImagePopup = document.querySelector(".popup_type_image .popup__close");
+closeImagePopup.addEventListener("click", function() {
+  imagePopup.style.display = "none";
+})
+
+imagePopup.addEventListener("click", function() {
+  imagePopup.style.display = "none";
+})
