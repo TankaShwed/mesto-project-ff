e7aadf1723548151f17cf67dafb10560731ddbe8
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-01-24 16:56:08 +0300

    добавление карточки

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ const editButton = content.querySelector(".profile__edit-button");
 const editPopup = document.querySelector(".popup_type_edit");
 const profileName = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-const formElement = document.querySelector(".popup__form");
+const formElementEditPopup = document.querySelector(".popup__form");
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_description");
 const closeEditPopup = document.querySelector(".popup__close");
 const contentPopup = document.querySelectorAll(".popup__content");
+
+const formElementAddPopup = document.querySelector(".popup_type_new-card .popup__form");
+const cardNameInput = document.querySelector(".popup__input_type_card-name");
+const cardImageInput = document.querySelector(".popup__input_type_url");
 
 const imagePopup = document.querySelector(".popup_type_image");
 
@@ -24,8 +28,9 @@ function makeCard(name, link) {
   cardElement.querySelector(".card__image").src = link;
   cardElement.querySelector(".card__image").alt = name;
 
-  delButton.addEventListener("click", function () {
+  delButton.addEventListener("click", function (evt) {
     onDelButton(cardElement);
+    evt.stopPropagation();
   });
 
   cardElement.addEventListener("click", function () {
@@ -60,7 +65,7 @@ function handleFormSubmit(evt) {
     editPopup.style.display = "none";
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+formElementEditPopup.addEventListener('submit', handleFormSubmit);
 
 closeEditPopup.addEventListener("click", function () {
   editPopup.style.display = "none";
@@ -75,6 +80,14 @@ contentPopup.forEach((element) => {
     evt.stopPropagation();
   });
 });
+
+formElementAddPopup.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  placesContent.prepend(makeCard(cardNameInput.value, cardImageInput.value));
+  addPopup.style.display = "none";
+  cardNameInput.value = '';
+  cardImageInput.value = '';
+})
 
 //add popup
 const addButton = content.querySelector(".profile__add-button");
