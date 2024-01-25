190de99273e74599172d4f1dea0bb1bcc8afd3da
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-01-25 10:10:10 +0300

    like

--- src/index.js
+++ src/index.js
@@ -13,34 +13,44 @@ const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_description");
 const closeEditPopup = document.querySelector(".popup__close");
 const contentPopup = document.querySelectorAll(".popup__content");
-
-const formElementAddPopup = document.querySelector(".popup_type_new-card .popup__form");
+const formElementAddPopup = document.querySelector(
+  ".popup_type_new-card .popup__form"
+);
 const cardNameInput = document.querySelector(".popup__input_type_card-name");
 const cardImageInput = document.querySelector(".popup__input_type_url");
-
 const imagePopup = document.querySelector(".popup_type_image");
 
-function makeCard(name, link) {
+function makeCard(name, link, onLikeButton) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const delButton = cardElement.querySelector(".card__delete-button");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const cardPicture = cardElement.querySelector(".card__image");
 
   cardElement.querySelector(".card__title").textContent = name;
-  cardElement.querySelector(".card__image").src = link;
-  cardElement.querySelector(".card__image").alt = name;
+  cardPicture.src = link;
+  cardPicture.alt = name;
 
   delButton.addEventListener("click", function (evt) {
     onDelButton(cardElement);
     evt.stopPropagation();
   });
 
-  cardElement.addEventListener("click", function () {
+  cardPicture.addEventListener("click", function () {
     imagePopup.style.display = "flex";
     imagePopup.querySelector(".popup__image").src = link;
     imagePopup.querySelector(".popup__image").alt = name;
     imagePopup.querySelector(".popup__caption").innerHTML = name;
   });
 
+  likeButton.addEventListener("click", function () {
+    onLikeButton(likeButton);
+  });
+
   return cardElement;
+}
+
+function like(like) {
+  like.classList.toggle("card__like-button_is-active");
 }
 
 function onDelButton(card) {
@@ -48,7 +58,7 @@ function onDelButton(card) {
 }
 
 initialCards.forEach((item) => {
-  placesContent.append(makeCard(item.name, item.link));
+  placesContent.append(makeCard(item.name, item.link, like));
 });
 
 //edit popup
@@ -56,16 +66,18 @@ editButton.addEventListener("click", function () {
   editPopup.style.display = "flex";
   nameInput.value = profileName.innerHTML;
   jobInput.value = profileDescription.innerHTML;
+  editPopup.classList.remove("popup_is-animated");
+  editPopup.classList.add("popup_is-opened");
 });
 
 function handleFormSubmit(evt) {
-    evt.preventDefault(); 
-    profileName.innerHTML = nameInput.value;
-    profileDescription.innerHTML = jobInput.value;
-    editPopup.style.display = "none";
+  evt.preventDefault();
+  profileName.innerHTML = nameInput.value;
+  profileDescription.innerHTML = jobInput.value;
+  editPopup.style.display = "none";
 }
 
-formElementEditPopup.addEventListener('submit', handleFormSubmit);
+formElementEditPopup.addEventListener("submit", handleFormSubmit);
 
 closeEditPopup.addEventListener("click", function () {
   editPopup.style.display = "none";
@@ -83,16 +95,20 @@ contentPopup.forEach((element) => {
 
 formElementAddPopup.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  placesContent.prepend(makeCard(cardNameInput.value, cardImageInput.value));
+  placesContent.prepend(
+    makeCard(cardNameInput.value, cardImageInput.value, like)
+  );
   addPopup.style.display = "none";
-  cardNameInput.value = '';
-  cardImageInput.value = '';
-})
+  cardNameInput.value = "";
+  cardImageInput.value = "";
+});
 
 //add popup
 const addButton = content.querySelector(".profile__add-button");
 const addPopup = document.querySelector(".popup_type_new-card");
-const closeAddPopup = document.querySelector(".popup_type_new-card .popup__close");
+const closeAddPopup = document.querySelector(
+  ".popup_type_new-card .popup__close"
+);
 addButton.addEventListener("click", function () {
   addPopup.style.display = "flex";
 });
@@ -107,17 +123,18 @@ addPopup.addEventListener("click", function () {
 
 document.body.addEventListener("keydown", function (e) {
   if (e.code == "Escape") {
-    editPopup.style.display = "none";
+    editPopup.classList.add("popup_is-animated");
+    editPopup.classList.remove("popup_is-opened");
+
     addPopup.style.display = "none";
     imagePopup.style.display = "none";
   }
 });
 
-
-
-
 //image popup
-const closeImagePopup = document.querySelector(".popup_type_image .popup__close");
+const closeImagePopup = document.querySelector(
+  ".popup_type_image .popup__close"
+);
 closeImagePopup.addEventListener("click", function () {
   imagePopup.style.display = "none";
 });
