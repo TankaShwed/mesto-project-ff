ccd7ad033d625e0d6b253dc535abf8c1ab93008b
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-01-27 01:48:50 +0300

    fix close by esc

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,11 +1,13 @@
 export function openModal(popup) {
   popup.classList.add("popup_is-opened");
   popup.classList.remove("popup_is-animated");
+  document.addEventListener("keydown", closeByEsc);
 }
 
 export function closeModal(popup) {
   popup.classList.add("popup_is-animated");
   popup.classList.remove("popup_is-opened");
+  document.removeEventListener("keydown", closeByEsc);
 }
 
 export function closePopupByOverlay(event) {
@@ -15,8 +17,8 @@ export function closePopupByOverlay(event) {
 }
 
 //ESC Close Popup
-document.addEventListener("keydown", function (e) {
+function closeByEsc(e) {
   if (e.code == "Escape") {
     closeModal(document.querySelector(".popup_is-opened"));
   }
-});
+}
