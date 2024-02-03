32517424adb116a68d4c77ea857a17332692552b
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-02-03 13:41:37 +0300

    input error name

--- src/index.js
+++ src/index.js
@@ -100,3 +100,77 @@ imagePopup.addEventListener("click", closePopupByOverlay);
 closeImagePopup.addEventListener("click", function () {
   closeModal(imagePopup);
 });
+
+//validation
+// Функция принимает массив полей
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {  // проходим по этому массиву // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся функция
+    // hasInvalidInput вернёт true
+    return !inputElement.validity.valid;
+  });
+}
+
+//// Функция принимает массив полей ввода
+// и элемент кнопки, состояние которой нужно менять
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {  // Если есть хотя бы один невалидный инпут
+    buttonElement.classList.add('button_inactive');  // сделай кнопку неактивной
+  } else {
+    buttonElement.classList.remove('button_inactive');  // иначе сделай кнопку активной
+  }
+}
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const selector = `.${inputElement.id}-error`;
+  console.log(selector)
+  const errorElement = formElement.querySelector(selector);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {  // Найдём все поля формы и сделаем из них массив
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));  // Найдём в текущей форме кнопку отправки
+  const buttonElement = formElement.querySelector('.popup__button');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);  // Вызовем toggleButtonState и передадим ей массив полей и кнопку
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+    // const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
+    // fieldsetList.forEach((fieldSet) => {
+    //   setEventListeners(fieldSet);
+    // });
+  });
+};
+
+enableValidation();
